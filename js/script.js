be4747c9a99c55c7bbad665d0f8637061967d92d
RHEAA RAO <RHEAA.RAO@JOURNALISM.CUNY.EDU>
2016-03-27 22:15:05 -0400

    reset

--- js/script.js
+++ js/script.js
@@ -1,7 +1,50 @@
+var submit = document.getElementById('submit');
+submit.onclick= tipCalculate;
+
 function tipCalculate(){
   console.log("TIP CALCULATE");
   //the function that will run 
   //when the submit button is clicked
   //as defined in the inline javascript
   //on index.html
+  var totalBill = document.getElementById('bill').value;
+  console.log(totalBill);
+
+  var tipPerc=getPercChecked();
+
+  if(!isNaN(totalBill)){
+  	var tipAmount = Math.floor(totalBill*tipPerc *100)/100;
+  var roundedAmount= tipAmount.toFixed(2);
+  var tipBox= document.getElementById('tipamount');
+  tipBox.innerHTML = '$' + roundedAmount;
+
+  } else {
+ alert('Please enter numbers only')
+  }
+
 }
+
+
+function getPercChecked(){
+	var percBtns= document.getElementsByClassName('tip-btn');
+	for (var i=0; i<percBtns.length;i++){
+		if(percBtns[i].checked){
+			return percBtns[i].value;
+		}
+	
+	}
+
+}
+
+
+
+function reset (){
+    document.getElementById('tipamount').innerHTML = "";
+    var choose = document.getElementsByClassName("tip-btn");
+   for(var i=0;i<choose.length;i++)
+    choose[i].checked = false;
+}
+
+document.getElementById('reset').onclick= reset;
+
+ 
